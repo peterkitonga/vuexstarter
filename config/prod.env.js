e85f60a8e446902471445f19640b91c808d5c20d
PeterKitonga <pkitonga.pk@gmail.com>
2018-08-16 21:52:43 +0300

    Required the dotenv library in the production env config js file

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,9 @@
 'use strict'
+
+// Require the dotenv plugin
+require('dotenv').config()
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_URL: JSON.stringify(process.env.API_URL)
 }
