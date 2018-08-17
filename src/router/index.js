187307137d094b8813d889b69ea3a17ac2df1e9c
PeterKitonga <pkitonga.pk@gmail.com>
2018-08-17 22:07:09 +0300

    Modified the home route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/views/guest/Home'
 
 import {store} from '../store'
 import * as TYPES from '../store/types'
@@ -12,8 +12,8 @@ const router = new VueRouter({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
     }
   ]
 })
